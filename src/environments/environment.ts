6836b233553234603a610ed297ea194dd61782d1
Jessie Woods <jessieallyn@gmail.com>
2022-01-31 22:45:37 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    API_KEY: "AIzaSyCh2KqYI7aXueWDYsgawAQg4cjar4xhXt8"
+  }
 };
 
 /*
